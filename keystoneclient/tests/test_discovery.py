4fd8f1e04175e73f39f5a5fb8180db26722a4021
Jamie Lennox <jamielennox@redhat.com>
2014-07-17 16:07:04 +1000

    Allow unauthenticated discovery
    The default state for session requests is that if there is an auth
    plugin available then it should include a token in any requests. This is
    a problem for cases where it is the authentication plugin itself trying
    to do discovery (like in the case of version independent plugins)
    because you end up in an infinite loop.
    
    Allow controlling the authenticated parameter on discovery requests.
    
    Change-Id: Ib5ab0a3a30fe79139b7b5dcaae698438281b6d36

--- keystoneclient/tests/test_discovery.py
+++ keystoneclient/tests/test_discovery.py
@@ -10,15 +10,19 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import uuid
+
 import six
 from testtools import matchers
 
 from keystoneclient import _discover
+from keystoneclient.auth import token_endpoint
 from keystoneclient import client
 from keystoneclient import discover
 from keystoneclient import exceptions
 from keystoneclient import fixture
 from keystoneclient.openstack.common import jsonutils
+from keystoneclient import session
 from keystoneclient.tests import utils
 from keystoneclient.v2_0 import client as v2_client
 from keystoneclient.v3 import client as v3_client
@@ -546,6 +550,34 @@ class ClientDiscoveryTests(utils.TestCase):
         disc = discover.Discover(auth_url=BASE_URL)
         self.assertRaises(exceptions.DiscoveryFailure,
                           disc.create_client, version=(3, 0))
+
+    def test_setting_authenticated_true(self):
+        self.requests.register_uri('GET', BASE_URL, status_code=300,
+                                   text=V3_VERSION_LIST)
+
+        token = uuid.uuid4().hex
+        url = 'http://testurl'
+        a = token_endpoint.Token(url, token)
+        s = session.Session(auth=a)
+
+        # will default to true as there is a plugin on the session
+        discover.Discover(s, auth_url=BASE_URL)
+
+        self.assertEqual(BASE_URL, self.requests.last_request.url)
+        self.assertRequestHeaderEqual('X-Auth-Token', token)
+
+    def test_setting_authenticated_false(self):
+        self.requests.register_uri('GET', BASE_URL, status_code=300,
+                                   text=V3_VERSION_LIST)
+
+        token = uuid.uuid4().hex
+        a = token_endpoint.Token('http://testurl', token)
+        s = session.Session(auth=a)
+
+        discover.Discover(s, auth_url=BASE_URL, authenticated=False)
+
+        self.assertEqual(BASE_URL, self.requests.last_request.url)
+        self.assertNotIn('X-Auth-Token', self.requests.last_request.headers)
 
 
 class DiscoverQueryTests(utils.TestCase):
